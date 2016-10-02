193b6a51b9f980d707a83ba8c36103c880a8abb7
wszlswork <18119514051@163.com>
2016-10-02 17:25:27 +0800

    list 完成

---
+++ www/src/Javascript/js/list.js
@@ -0,0 +1,55 @@
+/**
+ * Created by my on 2016/10/1.
+ */
+//main事件
+$("#main  .left ul li:not('#finally')").bind("mouseenter",function(){
+    var currentIndex = $(this).index();
+    $("#main .right ul li").eq(currentIndex).fadeTo("fast",0.3)
+})
+$("#main  .left ul li:not('#finally')").bind("mouseleave",function(){
+    var currentIndex = $(this).index();
+    $("#main .right ul li").eq(currentIndex).fadeTo("fast",1)
+})
+$("#main  .right ul li").bind("mouseenter",function(){
+    var currentIndex = $(this).index();
+    $("#main .left ul li a").eq(currentIndex).css("color","red")
+})
+$("#main .right ul li").bind("mouseleave",function(){
+    var currentIndex = $(this).index();
+    $("#main .left ul li a").eq(currentIndex).css("color","black")
+})
+//page Right last 改变当前页
+$("#main .right .last li").bind("click",function(){
+    //alert(1)
+    $("#main .right .last li").removeClass("current");
+    $(this).addClass("current");
+    $("#main  .left ul").slideUp("slow",function(){
+        $("#main  .left ul").slideDown("slow")
+    })
+    $("#main  .right .currentUl").fadeOut("slow",function(){
+        $("#main  .right .currentUl").fadeIn(3*1000)
+    })
+})
+    $(".list ul li").bind("mouseover",function(){
+        //alert(1)
+        $(this).css("border-color","red")
+    })
+    $(".list ul li").bind("mouseleave",function(){
+        //alert(1)
+        $(this).css("border-color","#ffffff")
+    })
+    //page 换页
+    $(".page ul li").bind("click",function(){
+        $(".list ul").fadeOut("slow",function(){
+            $(".list ul").fadeIn(3*1000)
+        })
+    })
+//$(function(){
+//    var currentUl = 0;
+//    showMainRightUl(currentUl);
+//    $("#main .right ")
+//
+//})
+//function showMainRightUl(currentUl){
+//    $("#main .right ul").eq(currentUl).fadeOut()
+//}
